61918a0fc69a9bcd4cf7e2ea569dc16d4b39825f
Saulius <you@example.com>
2020-10-24 11:39:11 +0300

    Update JS code for better readability

--- index.js
+++ index.js
@@ -5,32 +5,59 @@ const smallPictures = document.getElementById('smallPictures')
 function getData() {
   axios.get(url)
     .then(resp => {
-      let receivedData = resp.data
-      receivedData.map(picture => {
-        // Adding the big pictures
-        let div = document.createElement('div')
-        let att = document.createAttribute('class')
-        att.value = "swiper-slide"
-        div.setAttributeNode(att)
-        div.innerHTML = `<img loading="lazy" src="https://picsum.photos/id/${picture.id}/1920/1440">`
-        // Adding author
-        let author = `<p class='author'>${picture.author}</p>`
-        div.innerHTML = div.innerHTML + author
-        bigPicture.appendChild(div)
-        // Adding the small pictures
-        let divSmall = document.createElement('div')
-        let attSmall = document.createAttribute('class')
-        attSmall.value = "swiper-slide"
-        divSmall.setAttributeNode(attSmall)
-        divSmall.innerHTML = `<img src="https://picsum.photos/id/${picture.id}/400/300">`
-        smallPictures.appendChild(divSmall)
-      })
+      showBigPictures(resp.data)
+      showThumbnails(resp.data)
     })
     .catch(err => {
       console.log(err)
     })
 }
+
+function showBigPictures(responseData) {
+  responseData.map(picture => {
+    let bigPictureElement = document.createElement('div')
+    let attributeBig = document.createAttribute('class')
+    attributeBig.value = "swiper-slide"
+    bigPictureElement.setAttributeNode(attributeBig)
+    bigPictureElement.innerHTML = `<img loading="lazy" src="https://picsum.photos/id/${picture.id}/1920/1440"><p class='author'>${picture.author}</p>`
+    bigPicture.appendChild(bigPictureElement)
+  })
+}
+
+function showThumbnails(responseData) {
+  responseData.map(picture => {
+    let thumbnailElement = document.createElement('div')
+    let attributeThumb = document.createAttribute('class')
+    attributeThumb.value = "swiper-slide"
+    thumbnailElement.setAttributeNode(attributeThumb)
+    thumbnailElement.innerHTML = `<img src="https://picsum.photos/id/${picture.id}/400/300">`
+    smallPictures.appendChild(thumbnailElement)
+  })
+}
+
 getData()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 let galleryThumbs = new Swiper('.gallery-thumbs', {
